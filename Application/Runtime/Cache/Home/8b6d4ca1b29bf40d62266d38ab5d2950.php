b76f547d081edee5cc97c1c2dd79f53b72827258
nut1098 <1098129310@qq.com>
2017-04-19 22:40:36 +0800

    20170419

--- Application/Runtime/Cache/Home/8b6d4ca1b29bf40d62266d38ab5d2950.php
+++ Application/Runtime/Cache/Home/8b6d4ca1b29bf40d62266d38ab5d2950.php
@@ -13,17 +13,17 @@
 </head>
 <body>
 <div class="container">
-<form class="form-horizontal" style="width: 400px;">
+<form class="form-horizontal" style="width: 400px;" method="post" action="/Staff/addStaff">
  <div class="form-group">
     <label for="inputEmail3" class="col-sm-2 control-label">编号</label>
     <div class="col-sm-10">
-      <input type="email" class="form-control" id="inputEmail0">
+      <input type="text" class="form-control" id="inputEmail0" name="stNum">
     </div>
   </div>
   <div class="form-group">
     <label for="inputEmail3" class="col-sm-2 control-label">姓名</label>
     <div class="col-sm-10">
-      <input type="email" class="form-control" id="inputEmail3">
+      <input type="text" class="form-control" id="inputEmail3" name="stName">
     </div>
   </div>
   <div class="form-group">
@@ -31,13 +31,13 @@
     <div class="col-sm-10">
       	<div class="radio">
 	  <label>
-		<input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked>
+		<input type="radio" name="stSex" id="optionsRadios1" value="option1" checked>
 		男
 	  </label>
 		</div>
 		<div class="radio">
 		  <label>
-			<input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">
+			<input type="radio" name="stSex" id="optionsRadios2" value="option2">
 			女
 		  </label>
 		</div>
@@ -46,37 +46,37 @@
   <div class="form-group">
     <label for="inputPassword3" class="col-sm-2 control-label">年龄</label>
     <div class="col-sm-10">
-      <input type="text" class="form-control" id="inputPassword3">
+      <input type="text" class="form-control" id="inputPassword3" name="stAge">
     </div>
   </div>
   <div class="form-group">
     <label for="inputEmail3" class="col-sm-2 control-label">手机号码</label>
     <div class="col-sm-10">
-      <input type="email" class="form-control" id="inputEmail4">
+      <input type="text" class="form-control" id="inputEmail4" name="stTel">
     </div>
   </div>
   <div class="form-group">
     <label for="inputEmail3" class="col-sm-2 control-label">邮箱</label>
     <div class="col-sm-10">
-      <input type="email" class="form-control" id="inputEmail11">
+      <input type="email" class="form-control" id="inputEmail11" name="stEmail">
     </div>
   </div>
   <div class="form-group">
     <label for="inputEmail3" class="col-sm-2 control-label">籍贯</label>
     <div class="col-sm-10">
-      <input type="email" class="form-control" id="inputEmail12">
+      <input type="text" class="form-control" id="inputEmail12" name="stNativePlace">
     </div>
   </div>
   <div class="form-group">
     <label for="inputEmail3" class="col-sm-2 control-label">身份证号</label>
     <div class="col-sm-10">
-      <input type="email" class="form-control" id="inputEmail13">
+      <input type="text" class="form-control" id="inputEmail13" name="stIDCard">
     </div>
   </div>
   <div class="form-group">
     <label for="inputEmail3" class="col-sm-2 control-label">学历</label>
     <div class="col-sm-10">
-      	<select class="form-control">
+      	<select class="form-control"  name="stDegress">
 		  <option>本科</option>
 		  <option>研究生</option>
 		  <option>大专</option>
@@ -86,7 +86,7 @@
   <div class="form-group">
     <label for="inputEmail3" class="col-sm-2 control-label">所属部门</label>
     <div class="col-sm-10">
-      	<select class="form-control">
+      	<select class="form-control" name="stDepartment">
 		  <option>技术部</option>
 		  <option>行政部</option>
 		  <option>销售部</option>
@@ -96,22 +96,21 @@
   <div class="form-group">
     <label for="inputEmail3" class="col-sm-2 control-label">职位</label>
     <div class="col-sm-10">
-      <input type="email" class="form-control" id="inputEmail6">
+      <input type="text" class="form-control" id="inputEmail6" name="stDuties">
     </div>
   </div>
   <div class="form-group">
     <label for="inputEmail3" class="col-sm-2 control-label">入职日期</label>
     <div class="col-sm-10">
-      <input type="email" class="form-control" id="inputEmail7">
+      <input type="text" class="form-control" id="inputEmail7" name="stEntryDate">
     </div>
   </div>
   <div class="form-group">
-    <label for="inputEmail3" class="col-sm-2 control-label">职员身份</label>
-      <div class="col-sm-10">
-      	<select class="form-control">
+    <label for="inputEmail3" class="col-sm-2 control-label">在职状态</label>
+    <div class="col-sm-10">
+      	<select class="form-control" name="stJobState">
 		  <option>实习生</option>
 		  <option>正式员工</option>
-		  <option>临时工</option>
 		  <option>离职</option>
 		</select>
     </div>
@@ -122,6 +121,20 @@
     </div>
   </div>
 </form>
+<th scope="row">1</th>
+          <td><?php echo ($staff['stNum']); ?></td>
+          <td><?php echo ($staff['stName']); ?></td>
+          <td><?php echo ($staff['stSex']); ?></td>
+          <td><?php echo ($staff['stAge']); ?></td>
+          <td><?php echo ($staff['stTel']); ?></td>
+          <td><?php echo ($staff['stEmail']); ?></td>
+          <td><?php echo ($staff['stNativePlace']); ?></td>
+          <td><?php echo ($staff['stIDCard']); ?></td>
+          <td><?php echo ($staff['stDegress']); ?></td>
+          <td><?php echo ($staff['stDepartment']); ?></td>
+          <td><?php echo ($staff['stDuties']); ?></td>
+          <td><?php echo ($staff['stEntryDate']); ?></td>
+          <td><?php echo ($staff['stJobState']); ?></td>
 </div>
 </body>
 </html>
